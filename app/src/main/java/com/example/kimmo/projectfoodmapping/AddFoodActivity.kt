0c20ecb07f4c75aee8ad52a52778eb50c83c1c0d
Kim Möller <kim.moller2@gmail.com>
2018-12-08 19:25:42 +0200

    add onClick event to add food to database

--- app/src/main/java/com/example/kimmo/projectfoodmapping/AddFoodActivity.kt
+++ app/src/main/java/com/example/kimmo/projectfoodmapping/AddFoodActivity.kt
@@ -4,9 +4,10 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.TextView
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
+import java.math.BigDecimal
 
 class AddFoodActivity : AppCompatActivity() {
 
@@ -15,19 +16,28 @@ class AddFoodActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_food)
 
         val extraFoodName = intent.getStringExtra(EXTRA_FOOD_NAME)
-        val foodName = findViewById<TextView>(R.id.foodName).apply {
+        findViewById<TextView>(R.id.foodName).apply {
             text = extraFoodName
-        }
-        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
-        findViewById<TextView>(R.id.time_value).apply {
-            text = ZonedDateTime.now().format(formatter).toString()
         }
     }
 
     fun saveFood(view: View){
+        val food = getFood()
+        val db = AppDatabase.getDatabase(applicationContext)
+        db.foodDAO().insertFood(food)
         val intent = Intent(this, MainActivity::class.java).apply {
 
         }
         startActivity(intent)
     }
+
+    fun getFood(): Food {
+        val name = findViewById<TextView>(R.id.foodName).text.toString()
+        val ratingId = findViewById<RadioGroup>(R.id.food_rating_radio_group).checkedRadioButtonId
+        val comment = findViewById<EditText>(R.id.comment_edit_text).text.toString()
+        val price = findViewById<TextView>(R.id.price_input_field).text.toString().toDouble()
+        val type = findViewById<TextView>(R.id.type_input_field).text.toString()
+        return Food(name = name, rating = ratingId, comment = comment, price = price, type = type)
+    }
+
 }
