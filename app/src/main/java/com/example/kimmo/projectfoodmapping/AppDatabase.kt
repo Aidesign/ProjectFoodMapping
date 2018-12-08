cdaefccaceaa37f5fe09ea8bbf59bf82d8d0913f
Kim Möller <kim.moller2@gmail.com>
2018-12-08 23:22:52 +0200

    make database connection instance singleton

--- app/src/main/java/com/example/kimmo/projectfoodmapping/AppDatabase.kt
+++ app/src/main/java/com/example/kimmo/projectfoodmapping/AppDatabase.kt
@@ -10,16 +10,16 @@ abstract class AppDatabase : RoomDatabase() {
 
     companion object {
         @Volatile
-        private var INSTANCE: AppDatabase? = null
+                private var INSTANCE: AppDatabase? = null
 
-        fun getDatabase(context: Context): AppDatabase {
-            val tempInstance = INSTANCE
-            if(tempInstance != null) {
-                return tempInstance
-            }
-            synchronized(this) {
-                val instance = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "App_database").allowMainThreadQueries().build()
-                INSTANCE = instance
+                fun getDatabase(context: Context): AppDatabase {
+                    val tempInstance = INSTANCE
+                    if(tempInstance != null) {
+                        return tempInstance
+                    }
+                    synchronized(this) {
+                        val instance = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "App_database").allowMainThreadQueries().build()
+                        INSTANCE = instance
                 return instance
             }
         }
