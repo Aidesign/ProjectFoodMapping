433c23f569078508eff99d796a3154b45301e257
Kim Möller <kim.moller2@gmail.com>
2019-03-16 21:29:02 +0200

    make instance variabled private, remove unnecessary findViewById

--- app/src/main/java/com/example/kimmo/projectfoodmapping/activities/AddFoodActivity.kt
+++ app/src/main/java/com/example/kimmo/projectfoodmapping/activities/AddFoodActivity.kt
@@ -5,9 +5,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
 import android.widget.RadioButton
-import android.widget.RadioGroup
 import android.widget.TextView
 import com.example.kimmo.projectfoodmapping.R
 import com.example.kimmo.projectfoodmapping.persistence.config.AppDatabase
@@ -16,8 +14,8 @@ import kotlinx.android.synthetic.main.activity_add_food.*
 
 class AddFoodActivity : AppCompatActivity() {
 
-    lateinit var foodNameTextView: TextView
-    lateinit var foodTypeId: String
+    private lateinit var foodNameTextView: TextView
+    private lateinit var foodTypeId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,12 +50,11 @@ class AddFoodActivity : AppCompatActivity() {
     }
 
     fun getFood(): Food {
-        val name = findViewById<TextView>(R.id.foodName).text.toString()
-        val checkedRadioButtonId = findViewById<RadioGroup>(R.id.food_rating_radio_group).checkedRadioButtonId
+        val name = foodName.text.toString()
+        val checkedRadioButtonId = food_rating_radio_group.checkedRadioButtonId
         val ratingId = findViewById<RadioButton>(checkedRadioButtonId).text.toString().toInt()
-        val comment = findViewById<EditText>(R.id.comment_edit_text).text.toString()
-        val price = findViewById<EditText>(R.id.price_input_field).text.toString().toDouble()
-        val type = food_type_text_view.text.toString()
+        val comment = comment_edit_text.text.toString()
+        val price = price_input_field.text.toString().toDouble()
         return Food(name = name, rating = ratingId, comment = comment, price = price, typeId = foodTypeId, restaurantId = intent.getStringExtra(
             EXTRA_RESTAURANT_ID))
     }
